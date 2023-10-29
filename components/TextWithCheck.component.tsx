042feb3ffc4771777179f0e6474d083970565063
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-10-29 21:13:09 +0100

    feat: cambio en el estilo de los checks en puedo ayudarte

--- components/TextWithCheck.component.tsx
+++ components/TextWithCheck.component.tsx
@@ -7,10 +7,10 @@ export function TextWithCheckComponent({ text = '' }) {
     flex flex-row items-center gap-4 
     min-[1023px]:w-[450px] w-[300px]
     '>
-      <div className='p-3 rounded-full bg-whiteColor'>
-        <IconCheck className='text-secondaryColor2' />
+      <div className='p-1 rounded-full bg-whiteColor'>
+        <IconCheck className='text-secondaryColor1' size={20} />
       </div>
-      <div className='min-[1023px]:text-[16px] text-[14px]'>{text}</div>
+      <div className='text-[16px]'>{text}</div>
     </div>
   )
 }
