1ea791d6d9070015448eeb101499adddfc97f557
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-11-01 10:51:28 +0100

    feat: menu top se cierra si se clicka fuera del menu top

--- containers/MenuTop/MenuTop.container.tsx
+++ containers/MenuTop/MenuTop.container.tsx
@@ -31,7 +31,7 @@ export function MenuTop() {
   return (
     <>
       {/* PARTE MOVIL */}
-      <div className={'w-full flex flex-row justify-center items-center fixed z-20 h-14 text-xs min-[1023px]:bg-transparent bg-primaryColor3'}>
+      <div className={'w-full flex flex-row justify-center items-center fixed z-40 h-14 text-xs min-[1023px]:bg-transparent bg-primaryColor3'}>
         <Image
           src={`${basePath}images/logo.png`}
           width={100}
@@ -55,16 +55,16 @@ export function MenuTop() {
       </div>
       {/* PARTE PC */}
       <div className={`
-      ${scrolled > 0 ? style.menu_top_down : 'py-2 ease-in-out duration-300'}
-      ${menuOpen ? style.menu_top_down : style.menu_top_down_closed}
-      ${AlataFont.className}
-      flex items-center justify-between fixed z-20
-      min-[1023px]:w-full w-full
-      min-[1023px]:flex-row flex-col
-      min-[1023px]:top-0 top-14
-      min-[1023px]:bg-primaryColor3 bg-primaryColor3Soft
-      min-[1023px]:pr-5 p-0 '
-      min-[1023px]:text-sm text-xs`
+        ${scrolled > 0 ? style.menu_top_down : 'py-2 ease-in-out duration-300'}
+        ${menuOpen ? style.menu_top_down : style.menu_top_down_closed}
+        ${AlataFont.className}
+        flex items-center justify-between fixed z-50
+        min-[1023px]:w-full w-full
+        min-[1023px]:flex-row flex-col
+        min-[1023px]:top-0 top-14
+        min-[1023px]:bg-primaryColor3 bg-primaryColor3Soft
+        min-[1023px]:pr-5 p-0 '
+        min-[1023px]:text-sm text-xs`
       }>
         <Image
           src={`${basePath}images/logo.png`}
@@ -73,18 +73,18 @@ export function MenuTop() {
           alt='Logo'
           className={`
           ${scrolled > 0 ? 'h-[45px] w-[170px]' : 'h-[55px] w-[200px]'}
-          ease-in duration-150
-          min-[1281px]:ml-[3vw] min-[1023px]:ml-[3vw] ml-0
-          min-[1023px]:flex hidden
+            ease-in duration-150
+            min-[1281px]:ml-[3vw] min-[1023px]:ml-[3vw] ml-0
+            min-[1023px]:flex hidden
           `}
         />
         <div className={`
         ${menuOpen ? 'gap-10' : 'gap-0'}
-        flex items-center text-semibold
-        min-[1023px]:flex-row flex-col
-        min-[1023px]:justify-end justify-start
-        min-[1290px]:gap-10 min-[1023px]:gap-8
-        min-[1290px]:text-[14px] min-[1146px]:text-[13px] min-[1023px]:text-[11px]
+          flex items-center text-semibold
+          min-[1023px]:flex-row flex-col
+          min-[1023px]:justify-end justify-start
+          min-[1290px]:gap-10 min-[1023px]:gap-8
+          min-[1290px]:text-[14px] min-[1146px]:text-[13px] min-[1023px]:text-[11px]
         `}>
           <a href={`${basePath}#Inicio`} onClick={() => setMenuOpen(false)}>
             <span className='text-textColor2 cursor-pointer hover:text-secondaryColor1 font-medium'>
@@ -111,6 +111,13 @@ export function MenuTop() {
           </div>
         </div>
       </div>
+      {/* CAPA INVISIBLE PARA PODER CERRAR MENU TOP AL CLICKAR FUERA DE ÉL */}
+      <div className='
+        bg-transparent z-20 h-[100vh] fixed top-0 w-[100vw]
+        min-[1023px]:hidden block
+      '
+      onClick={() => setMenuOpen(false)}
+      ></div>
     </>
   )
 }
