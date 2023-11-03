3f4974097af6b92fb883cad1bb38b610d2e2656a
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-11-03 19:11:44 +0100

    feat: mejora en diseño y mensaje de whatsapp

--- components/Whatsapp.component.tsx
+++ components/Whatsapp.component.tsx
@@ -3,8 +3,8 @@ import { IconBrandWhatsapp } from "@tabler/icons-react";
 export function WhatsappComponent() {
 
   return (
-    <a href='https://wa.me/613041566?text=¡Hola!.%20%20Quiero%20más%20información.' target='_blank' className='fixed bottom-6 right-3 bg-whatsapp flex flex-row items-center cursor-pointer p-3 rounded-xl'>
-      <IconBrandWhatsapp color="white" size={40} />
+    <a href='https://wa.me/613041566?text=¡Hola!%20%20Quiero%20más%20información.' target='_blank' className='fixed bottom-6 right-3 bg-whatsapp flex flex-row items-center cursor-pointer p-2 rounded-xl'>
+      <IconBrandWhatsapp color="white" size={30} />
       <span className="text-whiteColor text-lg font-bold"></span>
     </a>
   )
